17ed244c9b517c6286df842b60c7310501b09296
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2023-12-05 20:48:56 +0300

    Createboolean.js
    Signed-off-by: Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>

--- boolean.js
+++ boolean.js
@@ -1,6 +1,7 @@
 NOTES:
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
+    
 ```javascript
 function boolToWord( bool ) {
     if (bool == true){
