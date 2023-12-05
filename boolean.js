e0bb5a31e7b5ba3bbdeb5a070fe3a3248301248f
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2023-12-05 20:50:06 +0300

    Update boolean.js
    Signed-off-by: Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>

--- boolean.js
+++ boolean.js
@@ -1,7 +1,6 @@
 NOTES:
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-
-    
+	
 ```javascript
 function boolToWord( bool ) {
     if (bool == true){
