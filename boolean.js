4489b73e5addda36ae98399eab41a8ed58bc0210
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2023-12-05 20:42:32 +0300

    Create boolean.js

--- boolean.js
+++ boolean.js
@@ -1,3 +1,7 @@
+NOTES:
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+```javascript
 function boolToWord( bool ) {
     if (bool == true){
       return 'Yes';
@@ -5,3 +9,4 @@ function boolToWord( bool ) {
        return 'No';
      } 
   }
+  ```
